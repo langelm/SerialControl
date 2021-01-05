46e91ffc850775a7bb8ba940deef923b5b679362
langelm <1791132431@qq.com>
2021-01-05 19:18:13 +0800

    SerialControl-v1

---
+++ SerialControl/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace SerialControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(62, 63);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(152, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "扫描";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(299, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "打开串口";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(37, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(434, 65);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "串口";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(62, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "开";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(299, 163);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(157, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "关";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(25, 24);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(184, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "开";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(262, 24);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(157, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "关";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(62, 325);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(184, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "开";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(299, 325);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(157, 23);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "关";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(37, 139);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(434, 61);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "NO.1";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Location = new System.Drawing.Point(37, 223);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(433, 57);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "NO.2";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Location = new System.Drawing.Point(37, 297);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(434, 69);
+            this.groupBox4.TabIndex = 12;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "NO.3";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 4800;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 392);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox4);
+            this.Name = "Form1";
+            this.Text = "串口控制";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ SerialControl/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialControl
+{
+    public partial class Form1 : Form
+    {
+        //device1
+        const byte DeviceOpen1 = 0x01;
+        const byte DeviceClose1 = 0x81;
+        //device2
+        const byte DeviceOpen2 = 0x02;
+        const byte DeviceClose2 = 0x82;
+        //device3
+        const byte DeviceOpen3 = 0x03;
+        const byte DeviceClose3 = 0x83;
+        //serialPort Write Buffer
+        byte[] SerialPortDataBuffer = new byte[1];
+        public Form1()
+        {
+            InitializeComponent();//窗口构造
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SearchAndAddSerialToComboBox(serialPort1, comboBox1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Close();
+                }
+                catch { }
+                button2.Text = "打开串口";
+            }
+            else
+            {
+                try
+                {
+                    serialPort1.PortName = comboBox1.Text; //端口号
+                    serialPort1.Open();//打开端口
+                    button2.Text = "关闭串口";
+                }
+                catch 
+                {
+                    MessageBox.Show("串口打开失败", "错误");
+                }
+            }
+        }
+        private void SearchAndAddSerialToComboBox(SerialPort MyPort, ComboBox MyBox)//扫描
+        {
+            string[] MyString = new string[20];                         //将可用端口号添加到ComboBox
+            string Buffer;                                              //缓存
+            MyBox.Items.Clear();                                        //清空ComboBox内容
+            //int count = 0;
+            for (int i = 1; i < 20; i++)                                //循环这里只扫描1-19
+            {
+                try                                                     //核心原理是依靠try和catch完成遍历
+                {
+                    Buffer = "COM" + i.ToString();
+                    MyPort.PortName = Buffer;
+                    MyPort.Open();                                      //如果失败，后面的代码不会执行
+                    //MyString[count] = Buffer;                          
+                    MyBox.Items.Add(Buffer);                            //打开成功，添加至下俩列表
+                    MyPort.Close();                                     //关闭
+                    //count++;
+                }
+                catch//出错了什么也不做继续循环
+                {
+                }
+                MyBox.Text = MyString[0];
+            }
+        }
+
+
+        private void WriteByteToSerialPort(byte data)                   //单字节写入串口
+        {
+            byte[] Buffer = new byte[2] { 0x00, data };                       //定义数组
+            if (serialPort1.IsOpen)                                     //传输数据的前提是端口已打开
+            {
+                try
+                {
+                    serialPort1.Write(Buffer, 0, 2);                    //写数据
+                }
+                catch
+                {
+                    MessageBox.Show("串口数据发送出错，请检查.", "错误");//错误处理
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SearchAndAddSerialToComboBox(serialPort1, comboBox1);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WriteByteToSerialPort(DeviceOpen1);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            WriteByteToSerialPort(DeviceClose1);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            WriteByteToSerialPort(DeviceOpen2);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            WriteByteToSerialPort(DeviceClose2);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            WriteByteToSerialPort(DeviceOpen3);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            WriteByteToSerialPort(DeviceClose3);
+        }
+    }
+}
